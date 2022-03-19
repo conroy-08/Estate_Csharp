4c91066baf5796a2b548094bcb2e130c83926258
chienkapi2k1 <chienkapi2k1@gmail.com>
2022-03-19 20:59:25 +0700

    19/03 - Chien - Login,Logout,ChangePassword_1

---
+++ EstateProject/EstateProject/Models/PasswordModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EstateProject.Models
+{
+    public class PasswordModel
+    {
+        [Required(ErrorMessage = "Mời nhập mật khẩu mới")]
+        public String newPassword { get; set; }
+
+        [Required(ErrorMessage = "Mời nhập mật khẩu cũ")]
+        public String oldPassword { get; set; }
+
+        [NotMapped]
+        [Compare("newPassword", ErrorMessage = " Xác nhận mật khẩu sai")]
+        public String confirmPassword { get; set; }
+    }
+}
